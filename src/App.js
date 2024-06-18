ecf919ac2ed4aa034346a252336086bb5928c5bd
MOONCHILD2403 <yashasbajaj2403@gmail.com>
2024-06-18 19:50:08 +0530

    added signup using google

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
+import React from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Signup from './pages/Signup'; 
 import { Outlet } from 'react-router-dom';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
-     <Outlet />
-    </div>
+    <GoogleOAuthProvider clientId="153653751627-ekfrl8i33l5pctd36lg0bdgb04u6v9qc.apps.googleusercontent.com">
+      <div className="App">
+        <Outlet />
+      </div>
+        <Signup />
+      </GoogleOAuthProvider>
   );
 }
 
